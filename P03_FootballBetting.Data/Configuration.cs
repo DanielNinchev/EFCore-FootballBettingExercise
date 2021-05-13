d199578c792a9e593b7ee908dcbe992a3f210c88
Daniel Ninchev <daniel.ninchev@gmail.com>
2021-05-13 13:41:56 +0300

    Initial Commit

---
+++ P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+
+    //git ignore
+    internal static class Configuration
+    {
+        internal static string ConnectionString = @"Server=.;Database=FootballBettingSystem;Integrated Security=True;";
+    }
+}
